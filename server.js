5de29e7fecd9789b8d56cf2ee57b634c33d434b5
Jullian Abinsay <jullianabinsay@gmail.com>
2021-11-22 13:48:53 -0500

    Configures App Headers and sets Express to Use JSON

--- server.js
+++ server.js
@@ -1,9 +1,37 @@
-const express = require('express');
-const app = express();
+////////////    API Routing Imports    ////////////
+const express = require('express')
+const app = express()
+app.use(express.json())
 
+////////////    Add Headers    ////////////
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*')
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,x-access-token')
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true)
+
+  // Pass to next layer of middleware
+  next();
+})
+
+////////////    API Routes    ////////////
+
+///////  Root Route  ///////
 app.get('/', (req, res) => {
-  res.send('Hello from MERN Crud App!');
+  res.send('Hello from MERN Application!');
 });
+
+
+///////    Initialize API    ///////
 
 // Listen to the App Engine-specified port, or 8080 otherwise
 const PORT = process.env.PORT || 8080;
